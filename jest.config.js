43962630350380f1fa6bd646eeba0b5f735ec329
IvanPostu <ipostu20000127@gmail.com>
2021-05-09 11:56:12 +0300

    Test config and filenames changed.

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,9 @@
 module.exports = {
+  roots: ['<rootDir>/__tests__'],
   preset: 'react-native',
   setupFilesAfterEnv: ['@testing-library/jest-native/extend-expect'],
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
-  setupFiles: ['<rootDir>/jest-setup.js'],
+  setupFiles: ['<rootDir>/__tests__/jest-setup.js'],
+  testMatch: ['**/?(*.)+(spec|test).+(ts|tsx|js)'],
+  testPathIgnorePatterns: ['/node_modules/'],
 };
